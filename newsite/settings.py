d97c7cf88407cb07bd568ee6c3a36553733e19a1
kyere7 <109560030+kyere7@users.noreply.github.com>
2022-07-30 15:41:12 +0000

    Update settings.py

--- newsite/settings.py
+++ newsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i+vr8q!t0weh*__qq-wccy@3-n%nt@cuq)^r)6irh9na=#s08t'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
